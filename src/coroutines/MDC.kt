2d4291e97bfd7bc90a7c9f6db8411fdfe4bd9aa1
ivkustoff <kustoffigor@gmail.com>
2020-05-16 23:39:45 +0300

    link

--- src/coroutines/MDC.kt
+++ src/coroutines/MDC.kt
@@ -7,6 +7,7 @@ import org.slf4j.MDC
 import kotlin.math.log
 
 // here is how to do MDC-logging with coroutines
+// https://medium.com/@elizarov/phantom-of-the-coroutine-afc63b03a131
 
 suspend fun doSomeWork() {
     delay(1000)
